d3cff931ff4016ca9586b7b75b3ab6b3209f7124
ArllanFelipe Santigo Correia <arllansantiago@gmail.com>
2023-11-22 11:00:02 -0300

    Nome do Usuario sera exibido abaixo da Letra Ini.

--- src/app/shared/constants/app-const.ts
+++ src/app/shared/constants/app-const.ts
@@ -1,5 +1,5 @@
 
-export class AppConst{    
+export class AppConst{
     public static UF:{descr:string}[] = [
           { descr: 'CE' }
         , { descr: 'RN' }
@@ -13,13 +13,13 @@ export class AppConst{
 export const SYSDATA:{name:string ,version:string ,Sessions:{name:string, id:string}[]} = {
   name: `Portal de Serviços`, version: `1.0.0`
   , Sessions: [
-    { name: 'TRF-5ª Região',id:'TR'}
+    { name: 'ASC-Title',id:'TR'}
     , { name: 'Seção do Ceara', id: 'CE' }
     , { name: 'Seção do Rio Grande do Norte', id: 'RN' }
     , { name: 'Seção do Paraiba', id: 'PB' }
     , { name: 'Seção do Pernambuco', id: 'PE' }
     , { name: 'Seção do Alagoas', id: 'AL' }
     , { name: 'Seção do Sergipe', id: 'SE' }]
-}; 
+};
 
 export const msgNotFound = 'Dados não encontrados!'
